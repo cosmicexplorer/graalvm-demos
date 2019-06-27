7d65a7bb10cfe0c1faeee85f3c266bba9bc6be3a
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-06-27 14:12:53 -0700

    show failing scalac-image.sh script

--- scala-days-2018/scalac-native/scalac-image.sh
+++ scala-days-2018/scalac-native/scalac-image.sh
@@ -1,15 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
-[ -z "$GRAALVM_HOME" ] && echo "GRAALVM_HOME is currently not set. This script will not work." && exit 1
-[ ! -f "$GRAALVM_HOME/bin/native-image" ] && echo "Can't find native-image in $GRAALVM_HOME. Check that the directory is valid." && exit 1
-[ -z "$SCALA_HOME" ] && echo "SCALA_HOME is currently not set. This script will not work." && exit 1
-SCALA_LIB="$SCALA_HOME/lib"
-if [ -d "$SCALA_HOME/libexec" ]; then
-   SCALA_LIB="$SCALA_HOME/libexec/lib"
+# Requires `coursier` and `mx` to be on the PATH, and $SVM_CHECKOUT should be pointed to the
+# substratevm/ subdirectory of the graal repo.
+
+set -euxo pipefail
+
+SCALA_VERSION='2.12.8'
+
+echo "scalaVersion := \"${SCALA_VERSION}\"" > scalac-substitutions/build.sbt
+
+pushd scalac-substitutions/
+sbt package
+popd
+
+function is_osx {
+  [[ "$(uname)" == 'Darwin' ]]
+}
+
+if is_osx; then
+  if [[ ! -d 'openjdk1.8.0_212-jvmci-20-b04/Contents/Home' ]]; then
+    curl -L -O https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-20-b04/openjdk-8u212-jvmci-20-b04-darwin-amd64.tar.gz
+    tar zxvf openjdk-8u212-jvmci-20-b04-darwin-amd64.tar.gz
+  fi
+  export JAVA_HOME="$(pwd)/openjdk1.8.0_212-jvmci-20-b04/Contents/Home"
+else
+  if [[ ! -d 'openjdk1.8.0_212-jvmci-20-b04' ]]; then
+    curl -L -O https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-20-b04/openjdk-8u212-jvmci-20-b04-linux-amd64.tar.gz
+    tar zxvf openjdk-8u212-jvmci-20-b04-linux-amd64.tar.gz
+  fi
+  export JAVA_HOME="$(pwd)/openjdk1.8.0_212-jvmci-20-b04"
 fi
-[ ! -d "$SCALA_LIB" ] && echo "Can't find jars in $SCALA_LIB. Check that the Scala instalation is correct." && exit 1
-for filename in $SCALA_LIB/*.jar; do
-    SCALA_LIB_CLASSPATH=$filename:$SCALA_LIB_CLASSPATH
-done
 
-$GRAALVM_HOME/bin/native-image -cp $SCALA_LIB_CLASSPATH:$PWD/scalac-substitutions/target/scala-2.12/scalac-substitutions_2.12-0.1.0-SNAPSHOT.jar scala.tools.nsc.Main -H:SubstitutionResources=substitutions.json,substitutions-2.12.json -H:ReflectionConfigurationFiles=scalac-substitutions/reflection-config.json -H:Name=scalac $@
+function merge_jars {
+  tr '\n' ':' | sed -re 's#:$##g'
+}
+
+SCALA_COMPILER_JARS="$(coursier fetch org.scala-lang:scala-{compiler,library,reflect}:"$SCALA_VERSION" | merge_jars)"
+
+mx -p "$SVM_CHECKOUT" build
+
+mx -p "$SVM_CHECKOUT" native-image \
+   -cp "$SCALA_COMPILER_JARS":$PWD/scalac-substitutions/target/scala-2.12/scalac-substitutions_2.12-0.1.0-SNAPSHOT.jar \
+   scala.tools.nsc.Main \
+   -H:SubstitutionResources=substitutions.json,substitutions-2.12.json \
+   -H:ReflectionConfigurationFiles=scalac-substitutions/reflection-config.json \
+   -H:Name=scalac \
+   -J-Xmx7g -O0 \
+   --verbose -H:+ReportExceptionStackTraces \
+   --no-fallback \
+   -Djava.io.tmpdir=/tmp \
+   --allow-incomplete-classpath --report-unsupported-elements-at-runtime \
+   --initialize-at-build-time=scala.runtime.StructuralCallSite \
+   --initialize-at-build-time=scala.runtime.EmptyMethodCache \
+   $@
